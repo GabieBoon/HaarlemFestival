e9bc1f23b689e43f2af634741a4f792d47fbd671
David <davidgroen96@gmail.com>
2018-12-11 21:52:15 +0100

    button hernoemd

--- app/Layouts/TicketsLayout.php
+++ app/Layouts/TicketsLayout.php
@@ -5,7 +5,8 @@
 
     <p> <?= $this->getWinkelwagen() ?> </p>
 
-    <form action="http://localhost<?= PROOT ?>Haarlem-Festival%20BitBucket/haarlem-festival/Tickets/clearWinkelwagen">
-        <button type = "submit"> clear winkelwagen</button>
+
+    <form action="http://localhost<?= PROOT ?>Haarlem-Festival%20BitBucket/haarlem-festival/Tickets/clearWinkelwagen/">
+        <button type = "submit"> reken winkelwagen af </button>
     </form>
 
