f381436676cd88a2d8d0df439e1298ff27d91843
Gabie Boon <gabieboon@gmail.com>
2018-12-12 13:21:25 +0100

    begin gemaakt aan header

--- app/Layouts/Header.php
+++ app/Layouts/Header.php
@@ -1,7 +1,21 @@
-<header id="<?= $this->class ?>">
-    <a href="<?= PROOT ?>Home/"> Home</a>
-    <a href="<?= PROOT ?>Dance/"> Dance</a>
-    <a href="<?= PROOT ?>Tickets/"> Tickets</a>
+<header id="test">
+    <nav class='page_section'>
+        <img class="nav-logo" src="<?= PROOT ?>images/HaarlemFestival-Logo-WT.png" alt="Haarlem Festival Logo" href="<?= PROOT ?>Home/" />
+        <ul>
+            <li><a href="<?= PROOT ?>Dance/">Dance</a></li>
+            <li><a href="<?= PROOT ?>Jazz/">Jazz</a></li>
+            <li><a href="<?= PROOT ?>Historic/">Historic</a></li>
+            <li><a href="<?= PROOT ?>Food/">Food</a></li>
+        </ul>
+        <ul class="nav-right">
+            <li><a href="<?= PROOT ?>Schedule/">Schedule</a></li>
+            <li><a href="<?= PROOT ?>Card/">Card</a></li>
+            <li>
+                <select>
+                    <option value="nederlands">Nederlands</option>
+                    <option value="engels">Engels</option>
+                </select>
+            </li>
+        </ul>
+    </nav>
 </header>
-
-
