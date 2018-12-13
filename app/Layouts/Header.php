15b10464c49ad4536eddb13c8c5d10e1ea4070b1
Gabie Boon <gabieboon@gmail.com>
2018-12-13 10:07:40 +0100

    header verbeterd - nav items op juiste plaats + logo hidden als header op de home page is

--- app/Layouts/Header.php
+++ app/Layouts/Header.php
@@ -1,7 +1,10 @@
 <header id="test">
-    <nav class='page_section'>
-        <img class="nav-logo" src="<?= PROOT ?>images/HaarlemFestival-Logo-WT.png" alt="Haarlem Festival Logo" href="<?= PROOT ?>Home/" />
-        <ul>
+    <img class="nav-logo <?php if($_SESSION['CurrentPage'] == 'Home'){echo 'hide';} ?>"   src="<?= PROOT ?>images/HaarlemFestival-Logo-WT.png" alt="Haarlem Festival Logo" href="<?= PROOT ?>Home/" />
+
+
+
+    <nav class='nav-wrapper'>
+        <ul class="nav-left">
             <li><a href="<?= PROOT ?>Dance/">Dance</a></li>
             <li><a href="<?= PROOT ?>Jazz/">Jazz</a></li>
             <li><a href="<?= PROOT ?>Historic/">Historic</a></li>
@@ -9,7 +12,7 @@
         </ul>
         <ul class="nav-right">
             <li><a href="<?= PROOT ?>Schedule/">Schedule</a></li>
-            <li><a href="<?= PROOT ?>Card/">Card</a></li>
+            <li><a href="<?= PROOT ?>Cart/">Cart</a></li>
             <li>
                 <select>
                     <option value="nederlands">Nederlands</option>
