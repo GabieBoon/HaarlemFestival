698c30f346c89bf3b4777555415f5d518f967f38
mitchtheuvenet <mitch.theuvenet@gmail.com>
2018-12-17 17:06:55 +0100

    added orderconfirm

--- app/Layouts/TicketsLayout.php
+++ app/Layouts/TicketsLayout.php
@@ -6,7 +6,7 @@
     <p><?= $this->getCart() ?></p>
 
 
-    <form action="<?= PROOT ?>OrderData">
+    <form method="post" action="<?= PROOT ?>OrderData">
         <button type="submit">reken Cart af</button>
     </form>
 
