2f118c2e318e08a7e0b0008a8c42ce54722bf388
David <davidgroen96@gmail.com>
2018-12-19 13:29:15 +0100

    sql joins for tickets toegevoegd
    staat voor nu in schedule

--- app/Models/ScheduleModel.php
+++ app/Models/ScheduleModel.php
@@ -43,4 +43,33 @@ class ScheduleModel extends ModelBase
                 join Artist as a on a.id = da.artistId";
         return $this->_db->query($sql)->getResult();
     }
+
+    public function getJazzTickets(){
+        $sql = "SELECT t.id, t.price, t.startTime, t.endTime, t.ticketsAvailable, t.event, t.isAllAccessTicket, v.name as venue, 
+                a.firstName, a.preposition, a.lastName FROM Ticket as t 
+                join JazzTicket as jt on t.id = jt.ticketId 
+                join Venue as v on v.id = jt.venueId join Artist as a on a.id = jt.artistId";
+        return $this->_db->query($sql)->getResult();
+    }
+
+    public function getFoodTickets(){
+        $sql = "SELECT t.id, t.price, t.startTime, t.endTime, t.ticketsAvailable, t.event, t.isAllAccessTicket, v.name as restaurant, 
+                ft.price12 FROM Ticket as t 
+                join FoodTicket as ft on t.id = ft.ticketId 
+                join Restaurant as r on r.id = ft.restaurantId 
+                join Venue as v on v.id = r.venueId";
+        return $this->_db->query($sql)->getResult();
+    }
+
+    public function getHistoricTickets(){
+        $sql = "SELECT t.id, t.price, t.startTime, t.endTime, t.ticketsAvailable, t.event, t.isAllAccessTicket, v.name as startLocation, 
+                ht.language, ht.isFamilyTicket FROM Ticket as t 
+                join HistoricTicket as ht on t.id = ht.ticketId 
+                join Venue as v on v.id = ht.venueId";
+        return $this->_db->query($sql)->getResult();
+    }
+
+
 }
+
+
