23113d9ccd188e70f304bd489c4e850915bd24be
Marco <M.R.vanderEijken@outlook.com>
2019-01-17 20:22:13 +0100

    david is cool

--- App/Models/HistoricModel.php
+++ App/Models/HistoricModel.php
@@ -15,7 +15,7 @@ class HistoricModel extends ModelBase
     public function getHistoricTickets()
     {
         $sql = "SELECT t.id, t.price, t.startTime, t.endTime, t.ticketsAvailable, t.event, t.isAllAccessTicket, v.name as startLocation, 
-                ht.language, ht.isFamilyTicket FROM Ticket as t 
+                ht.language, ht.familyTicketPrice FROM Ticket as t 
                 join HistoricTicket as ht on t.id = ht.ticketId 
                 join Venue as v on v.id = ht.venueId";
         return $this->_db->query($sql)->getResult();
--- App/Views/Historic/HisScheduleView.php
+++ App/Views/Historic/HisScheduleView.php
@@ -14,44 +14,48 @@
 
     <div class="background-image">
 <?= $this->content('hissubnav'); ?>
-        <section>
+        <section class="hisSchedeule">
 
-        <nav>
-            <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                <a class="nav-item nav-link active" id="nav-26-tab" data-toggle="tab" href="#nav-26" role="tab" aria-controls="nav-26" aria-selected="true">26</a>
-                <a class="nav-item nav-link" id="nav-27-tab" data-toggle="tab" href="#nav-27" role="tab" aria-controls="nav-27" aria-selected="false">27</a>
-                <a class="nav-item nav-link" id="nav-28-tab" data-toggle="tab" href="#nav-28" role="tab" aria-controls="nav-28" aria-selected="false">28</a>
-                <a class="nav-item nav-link" id="nav-29-tab" data-toggle="tab" href="#nav-29" role="tab" aria-controls="nav-29" aria-selected="false">29</a>
-            </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-27" role="tabpanel" aria-labelledby="nav-26-tab">
+            <nav>
+                <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-26-tab" data-toggle="tab" href="#nav-26" role="tab" aria-controls="nav-26" aria-selected="true">26</a>
+                    <a class="nav-item nav-link" id="nav-27-tab" data-toggle="tab" href="#nav-27" role="tab" aria-controls="nav-27" aria-selected="false">27</a>
+                    <a class="nav-item nav-link" id="nav-28-tab" data-toggle="tab" href="#nav-28" role="tab" aria-controls="nav-28" aria-selected="false">28</a>
+                    <a class="nav-item nav-link" id="nav-29-tab" data-toggle="tab" href="#nav-29" role="tab" aria-controls="nav-29" aria-selected="false">29</a>
+                </div>
+            </nav>
 
-                <table>
-                    <?=$this->table->generateTable(26,10,27, "Historic", true, false, $this->languages, $this->historicTickets) ?>
-                </table>
+            <div class="hisSchedeule1">
 
-            </div>
-            <div class="tab-pane fade" id="nav-28" role="tabpanel" aria-labelledby="nav-27-tab">
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-26" role="tabpanel" aria-labelledby="nav-26-tab">
 
-                <table>
-                    <?=$this->table->generateTable(27,10,27, "Historic", true, false, $this->languages, $this->historicTickets) ?>
-                </table>
+                    <table>
+                        <table class="HistoricTable" ><?= $this->table->generateTable(26,10,27, "Historic", false, true,$this->languages, $this->historicTickets) ?> </table>
+                    </table>
 
-            </div>
-            <div class="tab-pane fade" id="nav-29" role="tabpanel" aria-labelledby="nav-28-tab">
+                </div>
+                <div class="tab-pane fade" id="nav-27" role="tabpanel" aria-labelledby="nav-27-tab">
 
-                <table>
-                    <?=$this->table->generateTable(28,10,27, "Historic", true, false, $this->languages, $this->historicTickets) ?>
-                </table>
+                    <table>
+                        <table class="HistoricTable"><?= $this->table->generateTable(27,10,27, "Historic", false, true,$this->languages, $this->historicTickets) ?> </table>
+                    </table>
 
-            </div>
-            <div class="tab-pane fade" id="nav-28" role="tabpanel" aria-labelledby="nav-29-tab">
+                </div>
+                <div class="tab-pane fade" id="nav-28" role="tabpanel" aria-labelledby="nav-28-tab">
 
-                <table>
-                    <?=$this->table->generateTable(29,10,27, "Historic", true, false, $this->languages, $this->historicTickets) ?>
-                </table>
+                    <table>
+                        <table class="HistoricTable"><?= $this->table->generateTable(28,10,27, "Historic", false, true,$this->languages, $this->historicTickets) ?> </table>
+                    </table>
 
+                </div>
+                <div class="tab-pane fade" id="nav-29" role="tabpanel" aria-labelledby="nav-29-tab">
+
+                    <table>
+                        <table class="HistoricTable"><?= $this->table->generateTable(29,10,27, "Historic", false, true,$this->languages, $this->historicTickets) ?> </table>
+                    </table>
+
+                </div>
             </div>
         </div>
 
