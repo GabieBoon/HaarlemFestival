e2c4e3c897e05a180c9b614b8de548f3c3307f3c
jasper <jasperstedema@hotmail.com>
2019-01-18 22:23:26 +0100

    fixed statisticspage frontend

--- App/Views/CMS/StatisticsView.php
+++ App/Views/CMS/StatisticsView.php
@@ -14,15 +14,160 @@
             <!-- <p>Choose an event from the menu</p> -->
         </div>
     </header>
-    <main id="pageMain" class="">
+    <main id="pageMain" class="d-flex flex-wrap">
 
     <?php $this->partial('CMS', 'statsWidget'); ?>
         
         
-        
-        <?= showStatsWidget('Event', 'Total tickets sold', 9658, 16000); ?>
-        <?= showStatsWidget('Event', 'Total unsold tickets', 6342, 16000); ?>
-        <?= showStatsWidget('Event', 'Total ticket revenue', 689960, 0, '€'); ?>
+
+
+            <?php 
+            $data0 = (object)[
+                'title' => 'Total unsold tickets',
+                'data' => [
+                    (object)[
+                        'name' => 'Total unsold tickets',
+                        'value' => 6342,
+                        'color' => 'Default'
+                    ],
+                    (object)[
+                        'name' => 'Total tickets sold',
+                        'value' => 9658,
+                        'color' => ''
+                    ]
+                ],
+                'prefix' => ''
+            ];
+            $data1 = (object)[
+                'title' => 'Total sold tickets',
+                'data' => [
+                    (object)[
+                        'name' => 'Total tickets sold',
+                        'value' => 9658,
+                        'color' => 'Default'
+                    ],
+                    (object)[
+                        'name' => 'Total unsold tickets',
+                        'value' => 6342,
+                        'color' => ''
+                    ]
+                ],
+                'prefix' => ''
+            ];
+            $data2 = (object)[
+                'title' => 'Total ticket revenue',
+                'data' => [
+                    (object)[
+                        'name' => 'Total ticket revenue',
+                        'value' => 689960,
+                        'color' => 'Default'
+                    ]
+                ],
+                'prefix' => '€'
+            ];
+            $data3 = (object)[
+                'title' => 'Ticket revenue',
+                'data' => [
+                    (object)[
+                        'name' => 'Dance ticket revenue',
+                        'value' => 565681,
+                        'color' => 'Dance'
+                    ],
+                    (object)[
+                        'name' => 'Jazz ticket revenue',
+                        'value' => 104198,
+                        'color' => 'Jazz'
+                    ],
+                    (object)[
+                        'name' => 'Food reservation revenue',
+                        'value' => 11753,
+                        'color' => 'Food'
+                    ],
+                    (object)[
+                        'name' => 'Historic ticket revenue',
+                        'value' => 1986,
+                        'color' => 'Historic'
+                    ]
+                ],
+                'prefix' => '€'
+            ];
+            $data4 = (object)[
+                'title' => 'Dance tickets',
+                'data' => [
+                    (object)[
+                        'name' => 'Dance sold tickets',
+                        'value' => 7823,
+                        'color' => 'Dance'
+                    ],
+                    (object)[
+                        'name' => 'Dance unsold tickets',
+                        'value' => 2577,
+                        'color' => ''
+                    ]
+                ],
+                'prefix' => ''
+            ];
+            $data5 = (object)[
+                'title' => 'Jazz tickets',
+                'data' => [
+                    (object)[
+                        'name' => 'Jazz sold tickets',
+                        'value' => 2153,
+                        'color' => 'Jazz'
+                    ],
+                    (object)[
+                        'name' => 'Jazz unsold tickets',
+                        'value' => 2197,
+                        'color' => ''
+                    ]
+                ],
+                'prefix' => ''
+            ];
+            $data6 = (object)[
+                'title' => 'Food total reservations',
+                'data' => [
+                    (object)[
+                        'name' => 'Food total reservations made',
+                        'value' => 947,
+                        'color' => 'Food'
+                    ],
+                    (object)[
+                        'name' => 'Food total open reservations',
+                        'value' => 79,
+                        'color' => ''
+                    ]
+                ],
+                'prefix' => ''
+            ];
+            $data7 = (object)[
+                'title' => 'Historic tickets',
+                'data' => [
+                    (object)[
+                        'name' => 'Historic sold tickets',
+                        'value' => 120,
+                        'color' => 'Historic'
+                    ],
+                    (object)[
+                        'name' => 'Historic unsold tickets',
+                        'value' => 360,
+                        'color' => ''
+                    ]
+                ],
+                'prefix' => ''
+            ];
+            
+
+
+            
+            //< ? =  
+
+            for ($i = 0; $i < 8; $i++) {
+                showStatsWidget(${'data' . $i});
+            }
+
+
+
+            ?>
     </main>
 </div>
 <?php $this->end(); ?>
